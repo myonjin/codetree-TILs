fb8816c1a0d7dcb77fbea76b6bc21bd38c590c63
myonjin <109326291+myonjin@users.noreply.github.com>
2024-05-08 23:09:32 +0900

    Title: 트리의 부모 노드, Time: 455ms, Memory: 32MB, Status: Passed - CodeTree

--- 240508/트리의 부모 노드/parent-node-of-the-tree.py
+++ 240508/트리의 부모 노드/parent-node-of-the-tree.py
@@ -1,1 +1,25 @@
-n = int(input())
+N = int(input())
+edges = [ [] for _ in range(N+1)]
+visited = [False] * (N+1)
+parents = [0]*(N+1)
+for _ in range(N-1):
+    # a,b=list(map(int,input().split()))
+    a,b=list(map(int,input().split()))
+    edges[a].append(b)
+    edges[b].append(a)
+
+
+def tree(x):
+
+    for y in edges[x]:
+        if visited[y] == False:
+            visited[y] = True
+            parents[y] = x
+            tree(y)
+visited[1]=True
+tree(1)
+# print(parents)
+parents.pop(0)
+parents.pop(0)
+for i in parents:
+    print(i)
